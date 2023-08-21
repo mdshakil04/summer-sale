2cf34fc75a2ace305b0342674008a1a230ad7e04
mdshakil04 <m.shakil04@yahoo.com>
2023-08-21 17:34:38 +0600

    code updated

--- js/index.js
+++ js/index.js
@@ -4,11 +4,14 @@ function addToCart(target){
     // console.log(cartValueElement)
     const cartValueNumber = parseFloat(cartValueElement);
     const totalPriceElement =document.getElementById('total-price');
+    const finalPriceField = document.getElementById('final-price')
     // console.log(totalPriceElement)
     const totalPriceString = totalPriceElement.innerText;
     const totalPriceValue = parseFloat(totalPriceString);
     const totalPrice = totalPriceValue + cartValueNumber;
     totalPriceElement.innerText = totalPrice;
+    finalPriceField.innerText = totalPrice;
+
     const buttonDiscount = document.getElementById('discount-btn');
     if(totalPrice >= 200){
         buttonDiscount.removeAttribute('disabled')
